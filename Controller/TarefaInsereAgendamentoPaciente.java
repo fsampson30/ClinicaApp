100d21263f1c15d7e68fbdfec69dd4922782340f
Flavio Sampson <71216861+fsampson30@users.noreply.github.com>
2020-09-13 11:41:06 -0300

    Tarefa assíncrona para inserção do agendamento

---
+++ Controller/TarefaInsereAgendamentoPaciente.java
@@ -0,0 +1,76 @@
+package br.com.sonus.sonuscliente.Controller;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.view.View;
+
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.MalformedURLException;
+import java.net.SocketTimeoutException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+
+import br.com.sonus.sonuscliente.Model.Agendamento;
+import br.com.sonus.sonuscliente.Model.Paciente;
+import br.com.sonus.sonuscliente.View.AgendamentoActivity;
+
+public class TarefaInsereAgendamentoPaciente extends AsyncTask<Agendamento,Void, String> {
+
+    private AgendamentoActivity context;
+    private CaminhoServlets caminho = new CaminhoServlets();
+
+    public TarefaInsereAgendamentoPaciente(AgendamentoActivity context) {
+        this.context = (AgendamentoActivity) context;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        context.barraCircular.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    protected String doInBackground(Agendamento... agendamentos) {
+        try {
+            URLConnection conexao = new URL(caminho.getCaminho()+"/ServletInsereAgendamento").openConnection();
+            conexao.setDoOutput(true);
+            conexao.setConnectTimeout(10000);
+            Agendamento agendamento = agendamentos[0];
+            String parametro = URLEncoder.encode(new Gson().toJson(agendamento, Agendamento.class), "UTF-8");
+
+            OutputStreamWriter localOutputStreamWriter = new OutputStreamWriter(conexao.getOutputStream());
+            localOutputStreamWriter.write(parametro.toString());
+            localOutputStreamWriter.close();
+
+            BufferedReader reader = null;
+            reader = new BufferedReader(new InputStreamReader(conexao.getInputStream()));
+            String resposta = URLDecoder.decode(reader.readLine(), "UTF-8");
+            Log.d("retorno", resposta);
+            reader.close();
+            return resposta;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (SocketTimeoutException e){
+            e.printStackTrace();
+            return "9";
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return "";
+
+    }
+
+    @Override
+    protected void onPostExecute(String resposta) {
+        context.barraCircular.setVisibility(View.GONE);
+        this.context.retornoTarefaExterna(resposta);
+
+    }
+}
