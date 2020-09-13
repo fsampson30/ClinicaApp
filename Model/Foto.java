0358bbdde8d53679f1ae0cc8b8feeaca13c11885
Flavio Sampson <71216861+fsampson30@users.noreply.github.com>
2020-09-13 11:45:02 -0300

    Classes modelo

---
+++ Model/Foto.java
@@ -0,0 +1,23 @@
+package br.com.sonus.sonuscliente.Model;
+
+public class Foto {
+
+    private String cpfPaciente;
+    private byte [] foto;
+
+    public String getCpfPaciente() {
+        return cpfPaciente;
+    }
+
+    public void setCpfPaciente(String cpfPaciente) {
+        this.cpfPaciente = cpfPaciente;
+    }
+
+    public byte[] getFoto() {
+        return foto;
+    }
+
+    public void setFoto(byte[] foto) {
+        this.foto = foto;
+    }
+}
