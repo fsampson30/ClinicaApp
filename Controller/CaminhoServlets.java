a8a0718c94f6303a6499d6f92914ee4f41c8a0f2
Flavio Sampson <71216861+fsampson30@users.noreply.github.com>
2020-09-13 11:38:05 -0300

    Classe que retorna caminho do servlet

---
+++ Controller/CaminhoServlets.java
@@ -0,0 +1,12 @@
+package br.com.sonus.sonuscliente.Controller;
+
+public class CaminhoServlets {
+
+    private String caminho = "http://caminhodaaplicacao:8084/nomedoapp/";
+
+    public String getCaminho() {
+        return caminho;
+    }
+}
+
+
